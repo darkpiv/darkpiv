3d9bae38b305b3b418769afadf19709ca5fd0d47
nhat.hd <nhat.hd@geekup.vn>
2021-01-11 21:57:49 +0700

    .

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/darkpiv/darkpiv/darkpiv/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/darkpiv/darkpiv/darkpiv/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/darkpiv/darkpiv/darkpiv/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/darkpiv/darkpiv/darkpiv/node_modules/infima/dist/css/default/default.css"),
-  require("/home/darkpiv/darkpiv/darkpiv/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/darkpiv/darkpiv/darkpiv/src/css/custom.css"),
-];
