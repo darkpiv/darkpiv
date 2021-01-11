61cc48bdfadb09ebaf39b16d9b6cc782b795f159
nhat.hd <nhat.hd@geekup.vn>
2021-01-11 21:55:25 +0700

    add first init

--- blog/2020-11-28-my-first-post.md
+++ blog/2020-11-28-my-first-post.md
@@ -1,9 +1,9 @@
 ---
 title: 'My first post'
 description: May the force be with me. It's been a long time no writing anything. Now I want to be the writer of my own life.
-author_title: Docusaurus Core Team
-author_url: https://github.com/wgao19
-author_image_url: https://avatars1.githubusercontent.com/u/2055384?v=4
+author_title: darkpiv
+author_url: https://github.com/darkpiv
+author_image_url: /static/images/12.jpg
 date: '2020-11-28'
 modified_date: '2020-11-28'
 ---
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,30 +1,29 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'darkpiv',
+  tagline: 'The goal is not to be better than the other man, but your previous self.',
+  url: 'https://darkpiv.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  favicon: 'favicon.ico',
+  organizationName: 'github.com/darkpiv',
+  projectName: 'darkpiv',
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'darkpiv',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'darkpiv',
+        src: 'images/logo.png',
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
+          to: 'blog/',
+          activeBasePath: 'blog',
+          label: 'Blog',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/darkpiv/darkpiv',
           label: 'GitHub',
           position: 'right',
         },
@@ -34,36 +33,6 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
           title: 'More',
           items: [
             {
@@ -72,7 +41,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/darkpiv/darkpiv',
             },
           ],
         },
@@ -86,15 +55,11 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          editUrl: 'https://github.com/darkpiv/darkpiv/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          editUrl: 'https://github.com/darkpiv/darkpiv/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -102,4 +67,4 @@ module.exports = {
       },
     ],
   ],
-};
+}
